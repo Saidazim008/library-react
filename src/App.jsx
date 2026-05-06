da0f7217bba9132e8e1ca2836724bb776906662b
Saidazim008 <saidazibobomurodov@gmail.com>
2026-05-06 15:02:15 +0500

    tozalash

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,11 @@
-import { Routes, Route } from 'react-router-dom';
-import Header from "./component/header/Header";
-import Footer from "./component/footer/Footer";
-import Home from "./pages/home/Home";
-import './App.css';
+import React from 'react'
 
-
-function App() {
+const App = () => {
   return (
-    <div className="app-wrapper app-container " style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-
-
-      <Header />
-
-
-      <div style={{ display: 'flex', flex: 1 }}>
-
-        <main style={{ flex: 1, padding: '20px', backgroundColor: '#f4f4f4' }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-           
-          </Routes>
-        </main>
-
-      </div>
-
-
-      <Footer />
-
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
